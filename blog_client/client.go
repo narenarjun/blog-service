b68399ce031e47952dffd0ff483982f9f7e291c0
Narendran <narendev0610@gmail.com>
2020-06-18 15:42:53 +0530

    implemented readBlog client

--- blog_client/client.go
+++ blog_client/client.go
@@ -23,20 +23,54 @@ func main() {
 	c := blogpbgen.NewBlogServiceClient(conn)
 
 
-	// creating blog
-	fmt.Println("Creating the blog")
-	blog := &blogpbgen.Blog{
-		AuthorId: "Naren",
-		Title: "My first Blog",
-		Content: "Content of the first blog post",
-	}
-	createBlogres, err := c.CreateBlog(context.Background(),&blogpbgen.CreateBlogRequest{
-		Blog: blog,
-	})
-	if err != nil{
-		log.Fatalf("Unexpected Error : %v\n", err)
-		return
-	}
-	fmt.Printf("Blog has been created: %v\n",createBlogres)
+ blogID :=	createBlog(c)
+
+	readBlog(c, blogID)
 
 }
+
+func readBlog(c blogpbgen.BlogServiceClient ,bID string){
+
+	fmt.Println("Reading the blog")
+
+	// the following will show error because of the id is not in db
+	_, err := c.ReadBlog(context.Background(),&blogpbgen.ReadBlogRequest{BlogId:"gdilkasjgakjgd" })
+	if err != nil {
+		fmt.Printf("Error while reading blog: %v\n", err)
+	}
+
+	readBlogreq := &blogpbgen.ReadBlogRequest{
+		BlogId: bID,
+	}
+
+	res, err := c.ReadBlog(context.Background(),readBlogreq)
+
+	if err != nil {
+		fmt.Printf("Error while reading blog: %v\n", err)
+	}
+
+	fmt.Printf("Blog was read: %v\n", res)
+
+}
+
+
+func createBlog(c blogpbgen.BlogServiceClient ) string{
+		// creating blog
+		fmt.Println("Creating the blog")
+		blog := &blogpbgen.Blog{
+			AuthorId: "Arjun",
+			Title: "My Infinite Blog",
+			Content: "Content of the first blog post",
+		}
+		createBlogres, err := c.CreateBlog(context.Background(),&blogpbgen.CreateBlogRequest{
+			Blog: blog,
+		})
+		if err != nil{
+			log.Fatalf("Unexpected Error : %v\n", err)
+			return ""
+		}
+		fmt.Printf("Blog has been created: %v\n",createBlogres)
+
+		return createBlogres.GetBlog().GetId()
+}
+
